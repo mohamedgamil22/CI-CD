7873b59de153eb131be5b9a272be90dbd4632fc8
MOHAMED_GAMIL <MOHAMEDGAMIL22@GMAIL.COM>
2018-08-04 01:06:32 +0000

    test

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 #------------------------Running the script---------------#
 
 python3.6 testToken.py
-
+#test
 #------------------------Linting the script---------------#
 
 pylint -f parseable *.py | tee pylint.out
