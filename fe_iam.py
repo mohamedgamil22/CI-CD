a5b83f6fa4051d0700d1defa9976abb71e789dfa
MohamedGamil <mohamedgamil22@gmail.com>
2018-08-19 23:29:54 +0200

    Fixing linting errors

--- fe_iam.py
+++ fe_iam.py
@@ -1,44 +1,55 @@
+"""" a library for all the IAM related APIs.
+Can also be used for testing the service when called directly
+
+methods:
+  - gen_token(username, password) : generate a token from the IAM
+"""
 import os
-import feapi_lib
 import logging
+import feapi_lib
 
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)  # pylint: disable=C0103
+
 
 def main():
-    global logger
-    logging.basicConfig(filename = "iam.log",level = logging.INFO)
+    """ main function:
+    will trigger All IAM APIs as a test for the service"""
+
+    global logger  # pylint: disable=C0103,W0603
+    logging.basicConfig(filename="iam.log", level=logging.INFO)
     logger = logging.getLogger("fe_iam.py")
     try:
         logger.info("Reading Enviromental Varibles: UserName_API , Pass_API")
-        USER = os.environ['UserName_API']
-        PASSW = os.environ['Pass_API']
+        user = os.environ['UserName_API']
+        passw = os.environ['Pass_API']
     except KeyError:
-        print ("Please assign the username and password to Enviromental Global Varible")
-        print ("UserName_API , Pass_API")
+        print("Please assign the username and password to Enviromental Global Varible")
+        print("UserName_API , Pass_API")
         logging.critical("Failed to read UserName_API ,and/or, Pass_API")
         raise
     logger.info("Reading Varibles successeded")
 
-    tok,ex_date = gen_token(USER,PASSW)
-    print("Generated Token is:",tok)
-    print("Expiration date",ex_date)
+    tok, ex_date = gen_token(user, passw)
+    print("Generated Token is:", tok)
+    print("Expiration date", ex_date)
 
 
+def gen_token(username, password):
 
-def gen_token(username,password):
-
-
-    ''' Body of the request'''
+    # Body of the request
     logger.info("Starting to generate the token")
-    BODY= "{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"name\":\""+username+"\",\"password\":\""+password+"\",\"domain\":{\"name\":\"MohamedGamil\"}}}},\"scope\":{\"project\":{\"name\":\"eu-west-0\"}}}}"
+    body = "{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"name\":\"" + username + \
+        "\",\"password\":\"" + password + \
+        "\",\"domain\":{\"name\":\"MohamedGamil\"}}}},\"scope\":{\"project\":{\"name\":\"eu-west-0\"}}}}"  # pylint: disable=C0301
     try:
-        tokenReq = feapi_lib.call_feapi(Verb="POST", URI="/v3/auth/tokens", Body=BODY)
-        RESP_BODY = tokenReq.json()
-        expire_date = RESP_BODY['token']['expires_at']
-        MYTOKEN = (tokenReq.headers["X-Subject-Token"])
+        tokenReq = feapi_lib.call_feapi(
+            verb="POST", uri="/v3/auth/tokens", body=body)
+        resp_body = tokenReq.json()
+        expire_date = resp_body['token']['expires_at']
+        mytoken = (tokenReq.headers["X-Subject-Token"])
         logger.info("Generated Token Successfully")
-        return MYTOKEN,expire_date
+        return mytoken, expire_date
     except:
         print("Issue with request sent")
         logger.error("Failed to Obtain token")
@@ -46,7 +57,5 @@ def gen_token(username,password):
         raise
 
 
-
-
-if __name__  == "__main__":
+if __name__ == "__main__":
     main()
--- feapi_lib.py
+++ feapi_lib.py
@@ -5,27 +5,25 @@ methods:
   - call_feapi : to call a certai fe service api
   - get_srvcurl: to generate a service url
 """
-import requests
-import sys
 import logging
+import requests
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)  # pylint: disable=C0103
 
 
 REGION_LIST = ["eu-west-0", "na-east-0", "as-south-0"]
 SERVICE_LIST = ["iam", "ecs", "evs", "ims"]
 
 
-
-def call_feapi(Verb="GET", URI="/", Body=None, Token=None, Service="iam", Region="eu-west-0"):
-    """Function for Easy Calling FE APIs. 
+def call_feapi(verb="GET", uri="/", body=None, token=None, service="iam", region="eu-west-0"): # pylint: disable=R0913
+    """Function for Easy Calling FE APIs.
 
     Call the API of the mentioned services, on the Flexible Engine by using the specified URI.
 
     Args:
       Verb:  the verb of the API Call ( "GET","POST","PATCH","DELETE","PUT"), default value:"GET"
       URI: specify the path of the Call, e.g: /auth/tokens , Default Value: "/"
-      Body: a string with the body of the request. default: None
+      body: a string with the body of the request. default: None
       Service: the service used to send the API to, default: "iam"
       Region: define the FE Region to be used , defailt: "eu-west-0"
 
@@ -36,58 +34,59 @@ def call_feapi(Verb="GET", URI="/", Body=None, Token=None, Service="iam", Region
       TBD
 
     """
-    #First Generating the Service URL
-    URL = get_srvcurl(Service, Region)
-    ReqURL = URL + URI
+    # First Generating the Service URL
+    url = get_srvcurl(service, region)
+    req_url = url + uri
 
-    
-    #Checking if token is inserted, if not, do not add to Req Headers.
-    if Token is None:
-        Headers = {'Content-Type': 'application/json'}
+    # Checking if token is inserted, if not, do not add to Req headers.
+    if token is None:
+        headers = {'Content-Type': 'application/json'}
     else:
-        Headers = {'Content-Type': 'application/json', 'X-Auth-Token': Token}
-    logger.info(str("Sending API Call: "+ Verb + " , URL: "+ ReqURL+ " , Body: "+Body))
-    try: #raise all exception 
+        headers = {'Content-Type': 'application/json', 'X-Auth-Token': token}
+    logger.info(str("Sending API Call: " + verb +
+                    " , URL: " + req_url + " , Body: " + body))
+    try:  # raise all exception
 
-        #Making the request based on the giving verb.
-        if Verb == "GET":
-            req = requests.get(ReqURL, headers=Headers)
-        elif Verb == "POST":
-            req = requests.post(ReqURL, data=Body, headers=Headers)
-        elif Verb == "PATCH":
-            req = requests.patch(ReqURL, data=Body, headers=Headers)
+        # Making the request based on the giving verb.
+        if verb == "GET":
+            req = requests.get(req_url, headers=headers)
+        elif verb == "POST":
+            req = requests.post(req_url, data=body, headers=headers)
+        elif verb == "PATCH":
+            req = requests.patch(req_url, data=body, headers=headers)
         elif Verb == "DELETE":
-            req = requests.delete(ReqURL, Headers)
+            req = requests.delete(req_url, headers)
         elif Verb == "PUT":
-            req = requests.put(ReqURL, data=Body, headers=Headers)
+            req = requests.put(req_url, data=body, headers=headers)
         else:
-            logger.error(str("Used verb is wrong , Used verb: "+Verb))
-            raise TypeError("The Verb used is not supported, Please use on the following:" ,"GET , POST , PATCH , DELETE , PUT" )
-
-        req.raise_for_status() #to raise errors for unsuccessful status codes.
-
+            logger.error(str("Used verb is wrong , Used verb: " + Verb))
+            raise TypeError(
+                """The Verb used is not supported, Use on the following: "GET", "POST", "PATCH", "DELETE", "PUT" """)
+        req.raise_for_status()  # to raise errors for unsuccessful status codes.
 
     except Exception as ex:
-        logger.error("Failed Request, Error : %s" , ex)
+        logger.error("Failed Request, Error : %s", ex)
         raise
 
     return req
 
 
-
-def get_srvcurl(service , region):	
+def get_srvcurl(service, region):
     """ generate service URL for any FE service.
 
     Args:
       service: the service name, e.g: "iam","evs"
       region: region code, e.g: "eu-west-0","as-south-0"
 
-    """   
-    if not(region in REGION_LIST):
-        raise TypeError(" The region name is wrong, Availble regions:" , REGION_LIST)
-    #Confirm correct Service Name:
-    if not(service in SERVICE_LIST):
-        raise TypeError(" The service id is wrong, Availble Services are:" , SERVICE_LIST)
+    """
+    if not region in REGION_LIST:
+        raise TypeError(
+            " The region name is wrong, Availble regions:", REGION_LIST)
+    # Confirm correct Service Name:
+    if not service in SERVICE_LIST:
+        raise TypeError(
+            " The service id is wrong, Availble Services are:", SERVICE_LIST)
     else:
-        service_url = "https://" + service + "." + region + ".prod-cloud-ocb.orange-business.com"
+        service_url = "https://" + service + "." + \
+            region + ".prod-cloud-ocb.orange-business.com"
         return service_url
